15121e4133ab7a7629893687a1598d530612e356
y0k0s <kurpatowph@yandex.ru>
2026-04-08 16:55:44 +0300

    .

---
+++ src/main/java/org/example/models/OrderItem.java
@@ -0,0 +1,27 @@
+package org.example.models;
+
+public class OrderItem {
+    private int orderId;
+    private int productId;
+    private int quantity;
+
+    public int getOrderId() { return orderId; }
+
+    public void setOrderId(int orderId) { this.orderId = orderId; }
+
+    public int getProductId() { return productId; }
+
+    public void setProductId(int productId) { this.productId = productId; }
+
+    public int getQuantity() { return quantity; }
+
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "orderId=" + orderId +
+                ", productId=" + productId +
+                ", quantity=" + quantity + '}';
+    }
+}
