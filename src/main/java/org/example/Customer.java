ebbf9defabfb0dbfb3da2c55af98320dd898f0b2
y0k0s <kurpatowph@yandex.ru>
2026-04-08 16:00:36 +0300

    .

---
+++ src/main/java/org/example/Customer.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Customer {
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public int getCustomerId() { return customerId; }
+
+    public void setCustomerId(int customerId) { this.customerId = customerId; }
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email=" + email + '\'' + '}';
+    }
+}
