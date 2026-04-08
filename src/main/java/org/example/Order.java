5eb204b0b6894faf331243aa091aabdba1795a1c
y0k0s <kurpatowph@yandex.ru>
2026-04-08 15:50:32 +0300

    .

---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Date;
+
+public class Order {
+    private int orderId;
+    private int customerId;
+    private Date orderDate;
+
+    private int getOrderId() { return orderId; }
+
+    public void setOrderId(int orderId) { this.orderId = orderId; }
+
+    public int getCustomerId() { return customerId; }
+
+    public void setCustomerId(int customerId) { this.customerId = customerId; }
+
+    public Date getOrderDate() { return orderDate; }
+
+    public void setOrderDate(Date orderDate) { this.orderDate = orderDate; }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", customerId=" + customerId +
+                ", orderDate=" + orderDate + '}';
+    }
+}
